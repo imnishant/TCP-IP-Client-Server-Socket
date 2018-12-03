c749701b2fc0c9c05148bc4a1d0542069147c153
Nishant Sharma <nishantmaharishi@gmail.com>
2018-12-03 11:27:49 +0530

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -4,16 +4,21 @@ import java.util.*;
 
 public class Client {
 
-	public static void main(String[] args) throws UnknownHostException, IOException, NoSuchElementException {
-		String filePath = "Enter path of file";
-		Socket socket = new Socket("192.168.0.114",1235);
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		//String filePath = "src/Client.java";
+		
+		System.out.println("Enter name of the file which is there in the current directory");
+		//Current directory is your Project Folder, means the folder which contains src folder.
+		
+		String fileName = sc.nextLine();
+		Socket socket = new Socket("127.0.0.1",5000);
 		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		PrintStream p = new PrintStream(socket.getOutputStream());
 		
 		System.out.println("Client is Ready...");
 		
-		p.println(filePath);
+		p.println(fileName);
 		
 		String str = "";
 		while((str = br.readLine()) != null)
@@ -22,7 +27,6 @@ public class Client {
 		}
 		
 		System.out.println("Connection closed");
-		scan.close();
 		br.close();
 		socket.close();
 		p.close();
